bec1e30c63469ffd10228452c37582e29353631f
TomH-TRT <thomas.harper@digital.ncsc.gov.uk>
2019-12-02 12:35:33 +0000

    add button tests

--- src/components/Button/button.test.js
+++ src/components/Button/button.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from './index';
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<Button />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("should have a btn component", () => {
+  const wrapper = shallow(<Button name="Test" />);
+  expect(wrapper.find("button")).toHaveLength(1);
+
+  expect(wrapper.find("button").text()).toEqual("Test");
 });
